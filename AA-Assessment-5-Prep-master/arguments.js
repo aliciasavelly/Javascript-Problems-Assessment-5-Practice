9c2625aea146c80b4de8ccdc2f28eb33ff93d2cf
aliciasavelly <alicia.savelly@gmail.com>
2017-02-26 22:34:27 -0800

    complete arguments

--- AA-Assessment-5-Prep-master/arguments.js
+++ AA-Assessment-5-Prep-master/arguments.js
@@ -1,7 +1,12 @@
 // Write a sum function that takes any number of arguments:
 
 function mySum() {
-  args = Array.prototype.slice(arguments);
+  args = Array.prototype.slice.call(arguments);
+  sum = 0;
+  args.forEach(arg => {
+    sum += arg;
+  });
+  return sum;
 }
 
 console.log(mySum(1, 2, 3, 4) === 10);
