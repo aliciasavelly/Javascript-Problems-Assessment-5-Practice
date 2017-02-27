0c174faad6b890e77a9c0cf4f4a5249605a58d7e
aliciasavelly <alicia.savelly@gmail.com>
2017-02-26 22:46:06 -0800

    finish arrays

--- AA-Assessment-5-Prep-master/arrays.js
+++ AA-Assessment-5-Prep-master/arrays.js
@@ -2,16 +2,31 @@
 //
 
 function myUniq(arr) {
+  uniq = [];
+  arr.forEach(el => {
+    if (!uniq.includes(el)) {
+      uniq.push(el);
+    }
+  });
 
-}
+  return uniq;
+};
+
 console.log(myUniq([1, 1, 2, 2, 3, 3, 4, 4, 5, 5])); // [1,2,3,4,5]
 
 // Write a unique method for arrays directly on the array class
 //
 
 Array.prototype.myUnique = function () {
+  uniq = [];
+  this.forEach(el => {
+    if (!uniq.includes(el)) {
+      uniq.push(el);
+    }
+  });
 
-}
+  return uniq;
+};
 
 console.log([1, 1, 2, 2, 3, 3, 4, 4, 5, 5].myUnique()); // [1,2,3,4,5]
 
@@ -22,7 +37,17 @@ console.log([1, 1, 2, 2, 3, 3, 4, 4, 5, 5].myUnique()); // [1,2,3,4,5]
 //
 
 function twoSum(arr) {
+  result = [];
 
+  for(let i = 0; i < arr.length - 1; i++) {
+    for(let j = i + 1; j < arr.length; j++) {
+      if (arr[i] + arr[j] === 0) {
+        result.push([i, j]);
+      }
+    }
+  }
+
+  return result;
 }
 
 console.log(twoSum([-1, 0, 2, -2, 1])); // => [[0, 4], [2, 3]]
@@ -30,7 +55,17 @@ console.log(twoSum([-1, 0, 2, -2, 1])); // => [[0, 4], [2, 3]]
 // Write a Two sum
 
 Array.prototype.twoSum = function () {
+  result = [];
 
+  for(let i = 0; i < this.length - 1; i++) {
+    for(let j = i + 1; j < this.length; j++) {
+      if (this[i] + this[j] === 0) {
+        result.push([i, j]);
+      }
+    }
+  }
+
+  return result;
 }
 
 console.log([-1, 0, 2, -2, 1].twoSum()); // => [[0, 4], [2, 3]]
@@ -41,8 +76,20 @@ console.log([-1, 0, 2, -2, 1].twoSum()); // => [[0, 4], [2, 3]]
 //
 
 function myTranspose(arr) {
+  transposed = [];
 
-}
+  for(i = 0; i < arr[0].length; i++) {
+    transposed[i] = [];
+  }
+
+  arr.forEach(row => {
+    for(i = 0; i < row.length; i++) {
+      transposed[i].push(row[i]);
+    }
+  });
+
+  return transposed;
+};
 
 console.log(myTranspose([
    [0, 1, 2],
